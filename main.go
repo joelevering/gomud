677f100547cd56bc3aa4f78798244c9659fb02a6
Joe Levering <joe@reverb.com>
2016-06-14 09:01:44 -0500

    Add logging, timestamps, and auto-find local IP for hosting

--- main.go
+++ main.go
@@ -10,9 +10,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
 
 type client chan<- string
+
+const Port = "1919"
 
 var entering = make(chan client)
 var leaving = make(chan client)
@@ -22,8 +25,9 @@ func broadcaster(messages chan string) {
 	for {
 		select {
 		case msg := <-messages:
+			stamp := time.Now().Format(time.Kitchen)
 			for client := range clients {
-				client <- msg
+				client <- stamp + " " + msg
 			}
 		case client := <-entering:
 			clients[client] = true
@@ -44,6 +48,7 @@ func handleConn(conn net.Conn, messages chan string) {
 
 	// who := conn.RemoteAddr().String()
 	entering <- ch
+	log.Print("User logged in: " + who)
 	messages <- who + " has arrived!"
 
 	input := bufio.NewScanner(conn)
@@ -51,6 +56,7 @@ func handleConn(conn net.Conn, messages chan string) {
 		messages <- who + ": " + input.Text()
 	}
 
+	log.Print("User logged out: " + who)
 	messages <- who + " has left!"
 	leaving <- ch
 	conn.Close()
@@ -62,10 +68,24 @@ func clientWriter(conn net.Conn, ch <-chan string) {
 	}
 }
 
+func localIp() string {
+	addrs, _ := net.InterfaceAddrs()
+	for _, addr := range addrs {
+		ipnet := addr.(*net.IPNet)
+		if !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			return ipnet.IP.String()
+		}
+	}
+
+	return ""
+}
+
 func main() {
 	var messages = make(chan string)
 
-	listener, err := net.Listen("tcp", "1.1.1.1:1")
+	host := localIp() + ":" + Port
+	log.Print("Hosting on: " + host)
+	listener, err := net.Listen("tcp", host)
 
 	if err != nil {
 		log.Fatal(err)
